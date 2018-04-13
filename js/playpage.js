da466af99570f15657264e095ec5fb196f48e180
John Fu <john80510@gmail.com>
2018-04-13 18:16:04 +0000

    Fix strange play pause behavior, parse new json format (start_time, end_time)

---
+++ js/playpage.js
@@ -0,0 +1,76 @@
+var url = window.location.href;
+var id;
+
+url = window.location.href;
+url = url.replace('#', '');
+
+for (var i = url.length, ref = url.length; i >= 0; i = i - 1) {
+  if (url[i] == '/') {
+      id = url.substring(i + 1, ref-5);
+      break;
+  }
+}
+
+var subtitleName = "_"+id;
+console.log(subtitleName);
+var content = window[subtitleName];
+//var contentSize = content.length;
+
+$(window).on('load', function() {
+    content.forEach(function(element, index) {
+        var link = $(document.createElement('a'));
+        $("#subtitle_block").append("<a id='" + index + "'' class='subtitle' href='#' >" + element["text"] + "</a><br>");
+    });
+});
+
+var tag = document.createElement('script');
+tag.src = "http://www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+var player;
+var timeoutMillsec = 0;
+var timer;
+var stopTime = -1; 
+var startTime = -1;
+var repeat = false;
+var subClicked = false;
+
+function onYouTubePlayerAPIReady() {
+  player = new YT.Player('player', {
+    height: '390',
+    width: '640',
+    videoId: id,
+    events: {
+      //'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
+
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.PLAYING && subClicked == true) {
+    //debugger;
+    clearTimeout(timer);
+    var currentTime = player.getCurrentTime();
+    if(stopTime>currentTime*1000){
+      timeoutMillsec = stopTime - currentTime*1000; 
+      timer = setTimeout(pauseVideo, timeoutMillsec);
+    }
+  }
+}
+
+$(document).on('click', '.subtitle', function() {
+  startTime = content[this.id]["start_time"]
+  stopTime = content[this.id]["end_time"]
+  console.log("starttime: " + startTime);
+  console.log("stoptime: " + stopTime);
+  subClicked = true;
+  clearTimeout(timer);
+  player.pauseVideo();
+  player.seekTo(startTime/1000);
+  player.playVideo();
+});
+
+function pauseVideo() {
+  player.pauseVideo();
+}
