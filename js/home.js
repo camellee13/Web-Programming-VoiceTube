5f92ba19bdc98903899f4cb512073c25209ee1ac
Jo-Tung Li <camellee13@gmail.com>
2018-04-13 17:50:09 +0800

    homepage-nav

--- js/home.js
+++ js/home.js
@@ -1,20 +1,15 @@
-$('#myCarousel').carousel({
-  interval: 4000
-})
+function checkScroll(){
+    var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
 
-$('.carousel .item').each(function(){
-  var next = $(this).next();
-  if (!next.length) {
-    next = $(this).siblings(':first');
-  }
-  next.children(':first-child').clone().appendTo($(this));
-  
-  for (var i=0;i<2;i++) {
-    next=next.next();
-    if (!next.length) {
-      next = $(this).siblings(':first');
+    if($(window).scrollTop() > startY){
+        $('.navbar').addClass("scrolled");
+    }else{
+        $('.navbar').removeClass("scrolled");
     }
-    
-    next.children(':first-child').clone().appendTo($(this));
-  }
-});
+}
+
+if($('.navbar').length > 0){
+    $(window).on("scroll load resize", function(){
+        checkScroll();
+    });
+}
