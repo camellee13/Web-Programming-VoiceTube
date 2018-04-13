fe83e5ad87835cd8e6336eb52c72879ee4557801
John Fu <john80510@gmail.com>
2018-04-13 18:39:06 +0000

    Clean first version & Add some comment text

--- js/playpage.js
+++ js/playpage.js
@@ -1,45 +1,46 @@
-var url = window.location.href;
-var id;
-
-url = window.location.href;
-url = url.replace('#', '');
-
-for (var i = url.length, ref = url.length; i >= 0; i = i - 1) {
-  if (url[i] == '/') {
-      id = url.substring(i + 1, ref-5);
-      break;
-  }
-}
-
-var subtitleName = "_"+id;
-console.log(subtitleName);
-var content = window[subtitleName];
-//var contentSize = content.length;
-
-$(window).on('load', function() {
-    content.forEach(function(element, index) {
-        var link = $(document.createElement('a'));
-        $("#subtitle_block").append("<a id='" + index + "'' class='subtitle' href='#' >" + element["text"] + "</a><br>");
-    });
-});
-
-var tag = document.createElement('script');
-tag.src = "http://www.youtube.com/player_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+// some var
 var player;
 var timeoutMillsec = 0;
 var timer;
 var stopTime = -1; 
 var startTime = -1;
-var repeat = false;
 var subClicked = false;
+var v_id;
+var url
+
+// get video id
+url = window.location.href;
+url = url.replace('#', '');
+
+for (var i = url.length, ref = url.length; i >= 0; i = i - 1) {
+  if (url[i] == '/') {
+      v_id = url.substring(i + 1, ref-5);
+      break;
+  }
+}
+
+// load video's subtitles
+var subtitleName = "_" + v_id;
+console.log(subtitleName);
+var content = window[subtitleName];
+
+$(window).on('load', function() {
+    content.forEach(function(element, index) {
+        $("#subtitle_block").append("<a id='" + index + "'' class='subtitle' href='#' >" + element["text"] + "</a><br>");
+    });
+});
+
+// initial youtube iframe api
+var tag = document.createElement('script');
+tag.src = "http://www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 function onYouTubePlayerAPIReady() {
   player = new YT.Player('player', {
     height: '390',
     width: '640',
-    videoId: id,
+    videoId: v_id,
     events: {
       //'onReady': onPlayerReady,
       'onStateChange': onPlayerStateChange
@@ -59,6 +60,7 @@ function onPlayerStateChange(event) {
   }
 }
 
+// listen if subtitle is clicked
 $(document).on('click', '.subtitle', function() {
   startTime = content[this.id]["start_time"]
   stopTime = content[this.id]["end_time"]
@@ -71,6 +73,7 @@ $(document).on('click', '.subtitle', function() {
   player.playVideo();
 });
 
+// pause video
 function pauseVideo() {
   player.pauseVideo();
 }
