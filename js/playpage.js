7a495951d4868b570b3265e275530381b027a6c0
John Fu <john80510@gmail.com>
2018-04-15 05:48:23 +0000

    Color sub & Scroll cub

--- js/playpage.js
+++ js/playpage.js
@@ -2,11 +2,13 @@
 var player;
 var timeoutMillsec = 0;
 var timer;
-var stopTime = -1; 
+var stopTime = -1;
 var startTime = -1;
 var subClicked = false;
 var v_id;
-var url
+var s_id = -1; // colored sub id
+var url;
+var repeat = false;
 
 // get video id
 url = window.location.href;
@@ -14,20 +16,20 @@ url = url.replace('#', '');
 
 for (var i = url.length, ref = url.length; i >= 0; i = i - 1) {
   if (url[i] == '/') {
-      v_id = url.substring(i + 1, ref-5);
-      break;
+    v_id = url.substring(i + 1, ref - 5);
+    break;
   }
 }
 
 // load video's subtitles
 var subtitleName = "_" + v_id;
-console.log(subtitleName);
+// console.log(subtitleName);
 var content = window[subtitleName];
 
-$(window).on('load', function() {
-    content.forEach(function(element, index) {
-        $("#subtitle_block").append("<tr><td>" + "<a id='" + index + "'' class='subtitle' href='#' >" + "<i class='far fa-play-circle fa-lg'></i>" + "</a></td>" + "<td><a id='" + index + "'' class='subtitle' href='#' >" + element["text"] + "</a></td></tr>");
-    });
+$(window).on('load', function () {
+  content.forEach(function (element, index) {
+    $("#subtitle_block").append("<tr><td>" + "<a id='" + index + "'' class='subtitle' href='#' >" + "<i class='far fa-play-circle fa-lg'></i>" + "</a></td>" + "<td><a id='" + index + "'' class='subtitle' href='#' ><span id='subtitle_text_" + index + "' >" + element["text"] + "</span></a></td></tr>");
+  });
 });
 
 // initial youtube iframe api
@@ -49,31 +51,91 @@ function onYouTubePlayerAPIReady() {
 }
 
 function onPlayerStateChange(event) {
-  if (event.data == YT.PlayerState.PLAYING && subClicked == true) {
-    //debugger;
-    clearTimeout(timer);
+  clearTimeout(timer);
+  if (event.data == YT.PlayerState.PLAYING) {
     var currentTime = player.getCurrentTime();
-    if(stopTime>currentTime*1000){
-      timeoutMillsec = stopTime - currentTime*1000; 
-      timer = setTimeout(pauseVideo, timeoutMillsec);
+    if (currentTime * 1000 > startTime && currentTime * 1000 < stopTime) {
+      timeoutMillsec = stopTime - currentTime * 1000;
+      if (subClicked) {
+        if (repeat) {
+          timer = setTimeout(repeatVideo, timeoutMillsec);
+        } else {
+          timer = setTimeout(pauseVideo, timeoutMillsec);
+        }
+      }
+    }else{
+      subClicked = false;
+      id = whichSub(currentTime * 1000);
+      stopTime = content[id]["end_time"];
+      timeoutMillsec = stopTime - currentTime * 1000;
+      scrollSub(id);
+      colorSub(id);
+      timer = setTimeout(function() {sequenceSub(id+1);}, timeoutMillsec);
     }
   }
 }
 
 // listen if subtitle is clicked
-$(document).on('click', '.subtitle', function() {
+$(document).on('click', '.subtitle', function () {
   startTime = content[this.id]["start_time"]
   stopTime = content[this.id]["end_time"]
-  console.log("starttime: " + startTime);
-  console.log("stoptime: " + stopTime);
   subClicked = true;
+  colorSub(this.id);
+  scrollSub(this.id);
   clearTimeout(timer);
   player.pauseVideo();
-  player.seekTo(startTime/1000);
+  player.seekTo(startTime / 1000);
   player.playVideo();
 });
+
+// handle subtitle's color
+function colorSub(new_id) {
+  if (s_id != -1) {
+    $("#subtitle_text_" + s_id).css("background-color", "");
+  }
+  $("#subtitle_text_" + new_id).css("background-color", "green");
+  s_id = new_id;
+}
 
 // pause video
 function pauseVideo() {
   player.pauseVideo();
+  subClicked = false;
+}
+
+// repeat video
+function repeatVideo() {
+  player.pauseVideo();
+  player.seekTo(startTime / 1000);
+  player.playVideo();
+}
+
+// find sub id by time
+function whichSub(currentTime) {
+  var time = 0;
+  var id = 0;
+  while (time < currentTime) {
+    time = content[id]["end_time"];
+    id += 1;
+  }
+  id -= 1;
+  return id
+}
+
+// sequency change sub color by time
+function sequenceSub(id) {
+  startTime = content[id]["start_time"];
+  stopTime = content[id]["end_time"];
+  timeoutMillsec = stopTime - startTime;
+  colorSub(id);
+  scrollSub(id);
+  timer = setTimeout(function() {sequenceSub(id+1);}, timeoutMillsec);
+}
+
+// scroll sub to top
+function scrollSub(id){
+  var topPos = document.getElementById(id).parentNode.parentNode.offsetTop;
+  $(".panel").animate({
+    scrollTop : topPos
+  },250)
 }
